645c83c692411a7a60ea36e9a2843719b2fd0e98
Andrei Kazakov <andrey.kazakov@loudplay.io>
2021-03-13 22:17:57 +0300

    update clang-format

--- module/clang-format.cmake
+++ module/clang-format.cmake
@@ -1,11 +1,13 @@
-find_program(CLANG_FORMAT clang-format)
+option(ENABLE_CLANG_FORMAT "Enable syntax formatting" ON)
 
-if(NOT ${CLANG_FORMAT})
-	message(WARNING "Clang-fromat requested but executable not found")
-else()
-	message(STATUS "Clang-foramt is enabled")
+if(${ENABLE_CLANG_FORMAT})
+	find_program(CLANG_FORMAT clang-format)
 
-	file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
-
-	add_custom_target(${PROJECT_NAME}_clang_format COMMAND ${CLANG_FORMAT} -style=file -i ${ALL_SOURCE_FILES})
+	if(NOT CLANG_FORMAT)
+		message(WARNING "Clang-fromat requested but executable not found")
+	else()
+		message(STATUS "Clang-foramt is enabled")
+		file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+		add_custom_target(${PROJECT_NAME}-clang-format ALL COMMAND ${CLANG_FORMAT} -style=file -i ${ALL_SOURCE_FILES})
+	endif()
 endif()
