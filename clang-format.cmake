e5dc35ffa07912d59afac692994284f7f08eded0
Андрей Казаков <863955c1@mail.ru>
2021-09-15 08:54:54 +0000

    update clang-format

--- clang-format.cmake
+++ clang-format.cmake
@@ -1,12 +1,16 @@
-find_program(CLANG_FORMAT clang-format)
+option(ENABLE_CLANG_FORMAT "Enable syntax formatting" OFF)
 
-if(NOT ${CLANG_FORMAT})
-	message(WARNING "Clang-fromat requested but executable not found")
-else()
-	message(STATUS "Clang-foramt is enabled")
+if(${ENABLE_CLANG_FORMAT})
+	find_program(CLANG_FORMAT clang-format)
 
-	file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.hpp *.c *.h)
+	if(NOT CLANG_FORMAT)
+		message(WARNING "clang-fromat requested but executable not found")
+	else()
+		message(STATUS "clang-foramt is enabled")
 
-	add_custom_target(${PROJECT_NAME}_clang_format COMMAND ${CLANG_FORMAT} -style=file -i ${ALL_SOURCE_FILES})
-	set_target_properties(${PROJECT_NAME}_clang_format PROPERTIES EXCLUDE_FROM_ALL ON)
-endif()
+		file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.hpp *.c *.h)
+
+		add_custom_target(${PROJECT_NAME}_clang_format ALL COMMAND ${CLANG_FORMAT} -style=file -i ${ALL_SOURCE_FILES})
+		set_target_properties(${PROJECT_NAME}_clang_format PROPERTIES EXCLUDE_FROM_ALL ON)
+	 endif()
+ endif()
