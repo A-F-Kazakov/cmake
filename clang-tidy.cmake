823a9c57e9a9b4dd71d1ff53d3dc62dce87b6bbc
Андрей Казаков <863955c1@mail.ru>
2023-06-25 10:56:29 +0300

    update clang-tidy module

--- clang-tidy.cmake
+++ clang-tidy.cmake
@@ -1,4 +1,5 @@
 option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
+option(ENABLE_CLANG_TIDY_FIX_ERROR "Enable fixing mechanism" OFF)
 
 if(ENABLE_CLANG_TIDY)
 	find_program(CLANGTIDY clang-tidy)
@@ -8,7 +9,15 @@ if(ENABLE_CLANG_TIDY)
 	else()
 		message(STATUS "clang-tidy is enabled")
 
+		if(ENABLE_CLANG_TIDY_FIX_ERRORS)
+			set(FIX_ERRORS "--fix-errors")
+		endif()
+
+		if(CLANG_TIDY_FILTER_FILES)
+			set(FILTER_FILES "--line-filter=\"${CLANG_TIDY_FILTER_FILES}\"")
+		endif()
+
 		set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-		set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -p${CMAKE_BINARY_DIR} -fix)
+		set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -p ${CMAKE_BINARY_DIR} -fix ${FIX_ERRORS} ${FILTER_FILES})
 	endif()
 endif()
